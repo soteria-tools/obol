1f7144d8cf52f92d72dfbc2acd9664671a138daa
N1ark <opale.sjostedt@gmail.com>
2025-07-18 10:51:23 +0100

    Fix signature translation

--- src/translate/translate_functions.rs
+++ src/translate/translate_functions.rs
@@ -5,9 +5,11 @@
 
 extern crate stable_mir;
 
+use crate::translate::translate_body::lift_err;
+
 use super::translate_ctx::*;
-use charon_lib::{ast::*, raise_error, register_error};
-use stable_mir::{mir, ty};
+use charon_lib::ast::*;
+use stable_mir::mir;
 
 impl ItemTransCtx<'_, '_> {
     /// Translate a function's signature, and initialize a body translation context
@@ -20,43 +22,16 @@ impl ItemTransCtx<'_, '_> {
     ) -> Result<FunSig, Error> {
         let span = item_meta.span;
 
-        // self.translate_def_generics(span, def)?;
-        // is_unsafe = def.ty().kind().fn_def().unwrap().0.fn_sig().value.safety
-        let instance_ty = def.ty().kind();
-        let Some(rigid) = instance_ty.rigid() else {
-            raise_error!(self, Span::dummy(), "Instance without a rigid type?");
-        };
-        let signature = {
-            match rigid {
-                ty::RigidTy::FnDef(fndef, _) => Ok(fndef.fn_sig().value),
-                ty::RigidTy::Closure(_, args) => {
-                    let [.., fn_ty, _] = args.0.as_slice() else {
-                        raise_error!(self, Span::dummy(), "Weird closure gen args");
-                    };
-                    let fn_ty_kind = fn_ty.expect_ty().kind();
-                    let Some(ty::RigidTy::FnPtr(polysig)) = fn_ty_kind.rigid() else {
-                        raise_error!(
-                            self,
-                            Span::dummy(),
-                            "Expected a function type as the last argument of closure"
-                        );
-                    };
-                    Ok(polysig.value.clone())
-                }
-                _ => raise_error!(
-                    self,
-                    Span::dummy(),
-                    "Expected a function definition, found: {rigid:?}"
-                ),
-            }
-        }?;
+        let instance_abi = lift_err(def.fn_abi())?;
+
         // Translate the signature
-        let inputs: Vec<Ty> = signature
-            .inputs()
+
+        let inputs: Vec<Ty> = instance_abi
+            .args
             .iter()
-            .map(|ty| self.translate_ty(span, *ty))
+            .map(|arg| self.translate_ty(span, arg.ty))
             .try_collect()?;
-        let output = self.translate_ty(span, signature.output())?;
+        let output = self.translate_ty(span, instance_abi.ret.ty)?;
 
         Ok(FunSig {
             generics: GenericParams::empty(),
