aafbcbc3d65f76e9023b41b1779f7ce2c95c12d4
N1ark <opale.sjostedt@gmail.com>
2026-05-26 11:08:25 +0100

    remove more tests

--- tests/ui/unsupported/issue-79-bound-regions.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let slice: &[i32] = &[0];
-    let _ = slice.iter().next();
-}
--- tests/ui/unsupported/unbound-lifetime.rs
+++
@@ -1,5 +0,0 @@
-//@ known-failure
-// See https://github.com/AeneasVerif/charon/issues/306
-
-// This is incorrect code but the error is weird.
-fn get(_x: &'a u32) {}
